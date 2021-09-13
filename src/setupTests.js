f189ffb848ca6f5e276d750f0c13f6b4a32bd8f8
obotha <khyper234@gmail.com>
2021-09-13 12:14:46 +0300

    Add general config for enzyme.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// This file runs before each test
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
