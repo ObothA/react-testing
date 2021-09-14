c1a836497edb66ca586fa04f6ba9350a7d4d60a2
obotha <khyper234@gmail.com>
2021-09-14 08:26:11 +0300

    Mock usestate.

--- src/Input.test.js
+++ src/Input.test.js
@@ -19,3 +19,18 @@ test('Input renders without error.', () => {
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
 });
+
+describe('State controlled input field', () => {
+  test('State updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+});
